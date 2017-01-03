26e14b851c85c3ecfbb428474b471f6a3b4e11d0
William D. Gizzi <gizzidan@me.com>
2017-01-03 15:41:53 -0500

    blog 2

--- simplfit/urls.py
+++ simplfit/urls.py
@@ -22,5 +22,6 @@ from django.conf.urls import include
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('landingpage.urls')),
+    url(r'^blog/', include('blog.urls')),
 
 ]
